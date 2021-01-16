2920695568c6ec388c3acfba0bf02ae2aab8ee7e
Jane Bautina <janebarinova@gmail.com>
2021-01-16 15:00:43 -0800

    Edit: review comments

--- Stock.java
+++ Stock.java
@@ -1,15 +1,15 @@
 public class Stock {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length == 0)
+        if (prices == null || prices.length == 0)
           return 0;
         int maxP = Integer.MIN_VALUE;
         int localMin = prices[0];
         for (int i = 0; i < prices.length; i++) {
-            if(prices[i] >= localMin) {
-                if(prices[i] - localMin > maxP)
+            if (prices[i] >= localMin) {
+                if (prices[i] - localMin > maxP)
                     maxP = prices[i] - localMin;
             }
-            else{
+            else {
                localMin = prices[i];
             }
         }
